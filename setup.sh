2353aebe52f46d745d3487d1add6ea79239ca45b
Exyloss <antoninpons@protonmail.com>
2022-10-26 16:55:16 +0200

    commit

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo apt install --reinstall libappstream4
+sudo apt update
+sudo apt install isc-dhcp-server ssh
+sudo cp etc/default/isc-dhcp-server /etc/default/
+sudo cp etc/dhcp/dhcpd.conf /etc/dhcp/
+sudo echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
+sudo sysctl -p
