269fec0a83f5167d5f6d614bf72a3ffc854f2560
Exyloss <antoninpons@protonmail.com>
2022-11-16 16:45:12 +0100

    commit

--- setup.sh
+++ setup.sh
@@ -7,3 +7,5 @@ sudo cp etc/default/isc-dhcp-server /etc/default/
 sudo cp etc/dhcp/dhcpd.conf /etc/dhcp/
 sudo echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
 sudo sysctl -p
+sudo systemctl restart networking
+sudo systemctl restart isc-dhcp-server
