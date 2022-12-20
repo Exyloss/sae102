e4fa1289de6fbb9e299645d3e44f14cccde5891b
Exyloss <antonin.pons@protonmail.com>
2022-12-20 15:50:32 +0100

    commit

--- setup.sh
+++ setup.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
+# Executer le script en tant que root : chmod +x setup.sh && sudo ./setup.sh
 
-sudo apt -y install --reinstall libappstream4
-sudo apt -y update
-sudo apt -y install isc-dhcp-server ssh
-sudo cp etc/default/isc-dhcp-server /etc/default/
-sudo cp etc/dhcp/dhcpd.conf /etc/dhcp/
-sudo echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
-sudo sysctl -p
-sudo systemctl restart networking
-sudo systemctl restart isc-dhcp-server
-sudo iptables -t nat -A POSTROUTING -o ens3 -j MASQUERADE
+apt -y install --reinstall libappstream4
+apt -y update
+apt -y install isc-dhcp-server ssh nmap fping
+
+echo "INTERFACESv4='ens4'" > /etc/default/isc-dhcp-server
+
+echo "" > /etc/dhcp/dhcpd.conf
+echo "default-lease-time 600;\n\
+max-lease-time 7200;\n\
+subnet 192.168.36.0 netmask 255.255.255.0 {
+    range 192.168.36.2 192.168.36.254;\n\
+    option routers 192.168.36.1;\n\
+    option domain-name-servers 194.167.156.13;\n\
+}" >> /etc/dhcp/dhcpd.conf
+
+echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
+sysctl -p
+
+# On redémarre les services pour être sûr que les configurations sont bien appliquées
+systemctl restart networking
+systemctl restart isc-dhcp-server
+
+iptables -t nat -A POSTROUTING -o ens3 -j MASQUERADE
