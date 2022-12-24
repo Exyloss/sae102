933dc4681f412265d3fc8f6469ff3e2f0694d17b
Exyloss <antoninpons@protonmail.com>
2022-12-24 01:18:32 +0100

    commit

--- net.sh
+++ net.sh
@@ -4,13 +4,13 @@ printf 'Adresse IP du serveur (côté routeur):'
 read -r ip
 printf '\nAdresse IP du serveur (côté réseau local):'
 read -r ip_lan
-printf '\nAdresse IP de la passerelle:'
-read -r passerelle
+printf '\nAdresse IP du routeur:'
+read -r routeur
 printf '\nAdresse IP du serveur DNS:'
 read -r dns
 
 # On configure les deux cartes réseau
-nmcli con mod Connexion\ filaire\ 1 ipv4.addresses "$ip"/24 ipv4.gateway "$passerelle" ipv4.dns "$dns" ipv4.method manual
+nmcli con mod Connexion\ filaire\ 1 ipv4.addresses "$ip"/24 ipv4.gateway "$routeur" ipv4.dns "$dns" ipv4.method manual
 nmcli con mod Connexion\ filaire\ 2 ipv4.addresses "$ip_lan"/24 ipv4.gateway "$ip" ipv4.dns "$dns" ipv4.method manual
 
 # On relance les deux connexions
