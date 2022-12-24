8c02722eddc2e4c6a59e5da2bb5e6515caaf815a
Exyloss <antoninpons@protonmail.com>
2022-12-24 01:45:33 +0100

    commit

--- setup.sh
+++ setup.sh
@@ -7,14 +7,13 @@ apt -y install isc-dhcp-server ssh nmap fping
 
 echo "INTERFACESv4='ens4'" > /etc/default/isc-dhcp-server
 
-echo "" > /etc/dhcp/dhcpd.conf
 echo "default-lease-time 600;\n\
 max-lease-time 7200;\n\
 subnet 192.168.36.0 netmask 255.255.255.0 {
     range 192.168.36.2 192.168.36.254;\n\
     option routers 192.168.36.1;\n\
     option domain-name-servers 194.167.156.13;\n\
-}" >> /etc/dhcp/dhcpd.conf
+}" > /etc/dhcp/dhcpd.conf
 
 echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
 sysctl -p
