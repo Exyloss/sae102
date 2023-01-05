47c55e8135655af81c30e7a7233bd1b02187ffbc
Exyloss <antoninpons@protonmail.com>
2023-01-05 14:21:35 +0100

    commit

--- setup.sh
+++ setup.sh
@@ -13,9 +13,9 @@ echo "INTERFACESv4='ens4'" > /etc/default/isc-dhcp-server
 
 printf "default-lease-time 600;\n\
 max-lease-time 7200;\n\
-subnet $ip_lan.0 netmask 255.255.255.0 {
-    range $ip_lan.2 $ip_lan.254;\n\
-    option routers $ip_lan;\n\
+subnet $1.0 netmask 255.255.255.0 {
+    range $1.2 $1.254;\n\
+    option routers $1.1;\n\
     option domain-name-servers 194.167.156.13;\n\
 }\n" > /etc/dhcp/dhcpd.conf
 
