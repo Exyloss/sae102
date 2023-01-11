5906d643eb493cf1263eedbcba4f50c44af53fd0
Exyloss <antoninpons@protonmail.com>
2023-01-12 00:17:13 +0100

    commit

--- config_rpi.sh
+++ config_rpi.sh
@@ -13,39 +13,40 @@ lsblk
 printf "Disque de la carte SD (sans le /dev/):"
 read -r sd
 
+if grep -q "^/dev/$sd" /proc/mounts; then
+    sudo umount /dev/"${sd}"* 2>/dev/null
+fi
+
 # Ecriture de l'OS sur la carte SD
-echo "Ecriture de l'image sur la carte SD en cours..."
+echo "Ecriture de l'image sur la carte SD en cours, ne pas interrompre le programme..."
 sudo dd if=2022-09-22-raspios-bullseye-armhf-lite.img of=/dev/"$sd" bs=1M status=progress || exit 1
 
 # Montage de la carte SD
-if grep -q "^/dev/$sd" /proc/mounts; then
-    mpboot=$(grep "^/dev/${sd}1" /proc/mounts | cut -d ' ' -f 2)
-    mproot=$(grep "^/dev/${sd}2" /proc/mounts | cut -d ' ' -f 2)
-else
-    [ -d /mnt ] || sudo mkdir /mnt /mnt/boot
-    sudo mount /dev/"$sd"2 /mnt || exit 1
-    sudo mount /dev/"$sd"1 /mnt/boot || exit 1
-    mpboot="/mnt/boot"
-    mproot="/mnt"
-fi
-
+[ -d /mnt ] || sudo mkdir /mnt /mnt/boot
+sudo mount /dev/"$sd"2 /mnt || exit 1
+sudo mount /dev/"$sd"1 /mnt/boot || exit 1
 
 # Activation et configuration SSH
-sudo touch "${mpboot}/ssh"
-echo "PermitRootLogin no" | sudo tee -a "${mproot}/etc/ssh/sshd_config" || exit 1
+sudo touch /mnt/boot/ssh
+echo "PermitRootLogin no" | sudo tee -a /mnt/etc/ssh/sshd_config || exit 1
 
 # Mise à jour d'openssl
-distro=$(grep "^ID=" /etc/os-release | cut -d '=' -f 2 )
-case "$distro" in
-    "ubuntu"|"debian") sudo apt install openssl || exit 1 ;;
-    "arch") sudo pacman -Sy openssl ;;
-esac
-
+printf "Souhaitez-vous mettre à jour OpenSSL ? [O/n] "
+read -r confirm
+if [ "$confirm" = "o" ] || [ "$confirm" = "O" ]; then
+    distro=$(grep "^ID=" /etc/os-release | cut -d '=' -f 2 )
+    case "$distro" in
+        "ubuntu"|"debian") sudo apt -y install openssl || exit 1 ;;
+        "arch") sudo pacman -Sy openssl ;;
+    esac
+else
+    echo "OpenSSL ne sera pas mit à jour"
+fi
 # Configuration mot de passe pi
 echo "Veuillez rentrer le mot de passe de l'utilisateur pi"
 pass_hash=$(openssl passwd -6) || exit 1
-sudo sed -i "s|pi:\*:|pi:${pass_hash}:|g" "${mproot}/etc/shadow" || exit 1
+sudo sed -i "s|pi:\*:|pi:${pass_hash}:|g" /mnt/etc/shadow || exit 1
 
 # Démontage de la carte
-sudo umount -R "${mpboot}" || exit 1
-sudo umount -R "${mproot}" || exit 1
+sudo umount -R /mnt || exit 1
+echo "Installation terminée avec succès."
